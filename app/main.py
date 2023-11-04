853d5089aa1505b209939c210844ddcf3e26947c
narcis.fanica <narcis.fanica@getik.net>
2023-11-04 15:31:41 +0200

    Add GET endpoint to retrieve user by ID

--- app/main.py
+++ app/main.py
@@ -75,3 +75,13 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(user)
     return user
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserOut)
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+    return user
