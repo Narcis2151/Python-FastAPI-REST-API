82e78a06ca0dfa42e5029ee2ba63071741e5b0f1
narcis.fanica <narcis.fanica@getik.net>
2023-11-10 11:31:19 +0200

    Add authentication to user router

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,9 @@
+from typing import Annotated
 from fastapi import Depends, APIRouter, HTTPException, status
 from sqlalchemy.orm import Session
 from .. import models, schemas
 from ..database import get_db
+from ..oauth2 import get_current_user
 from ..utils import pwd_context
 
 router = APIRouter(prefix="/users", tags=["Users"])
@@ -17,8 +19,12 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return user
 
 
-@router.get("/{user_id}", response_model=schemas.UserOut)
-async def get_user(user_id: int, db: Session = Depends(get_db)):
+@router.get("/me", response_model=schemas.UserOut)
+async def get_user(
+    token_data: Annotated[schemas.TokenData, Depends(get_current_user)],
+    db: Session = Depends(get_db),
+):
+    user_id = token_data.user_id
     user = db.query(models.User).filter(models.User.user_id == user_id).first()
     if not user:
         raise HTTPException(
